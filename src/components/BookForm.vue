399ea55fca2512d6be8755c76030a7816e432426
treknuts <trevor.a.knutson@ndsu.edu>
2021-05-14 16:19:47 -0500

    Initial commit

---
+++ src/components/BookForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="book">
+    <h1>{{ msg }} &#x1F60A;</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookForm",
+  props: {
+    msg: String,
+    emote: String,
+  },
+};
+</script>
+
+<style scoped>
+.book {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  padding: 0;
+  margin: 0;
+  color: #42b983;
+}
+</style>
---
+++ src/views/Book.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="book">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <BookForm msg="Book the venue for your wedding here!" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BookForm from "@/components/BookForm.vue";
+
+export default {
+  name: "Book",
+  components: {
+    BookForm,
+  },
+};
+</script>
